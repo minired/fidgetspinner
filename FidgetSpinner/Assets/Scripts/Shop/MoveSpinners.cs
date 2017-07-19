1cee27cd901764ca960d372df07401ff4c82178e
YK <rlduf1028@naver.com>
2017-07-19 17:09:05 +0900

    Shop 스크롤뷰, 좌우 이동

---
+++ FidgetSpinner/Assets/Scripts/Shop/MoveSpinners.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSpinners : MonoBehaviour {
+
+    public float currentPosition;
+    int amountOfSpinners;
+    public SpringPanel panel;
+
+    void Start()
+    {
+        currentPosition = transform.localPosition.x;
+        amountOfSpinners = 4;
+
+    }
+    void Update()
+    {
+
+    }
+
+    void MoveRight()
+    {
+        currentPosition = panel.target.x;
+        if (currentPosition <= (amountOfSpinners - 1) * -500f)
+            return;
+        currentPosition -= 500f;
+        
+        Vector3 position = new Vector3(currentPosition, 0, 0);
+        SpringPanel.Begin(this.gameObject, position, 8f);
+    }
+    
+
+    void MoveLeft()
+    {
+        currentPosition = panel.target.x;
+        if (currentPosition >= 0f)
+            return;
+        currentPosition += 500f;
+
+        Vector3 position = new Vector3(currentPosition, 0, 0);
+        SpringPanel.Begin(this.gameObject, position, 8f);
+    }
+    
+}
