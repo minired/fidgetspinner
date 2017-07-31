fdefb10a03088992535ffa527b42e856f5364660
YK <rlduf1028@naver.com>
2017-08-01 01:54:18 +0900

    Shop UI 글씨체, 색깔, 배치 등등 조정

--- FidgetSpinner/Assets/Scripts/Shop/MoveSpinners.cs
+++ FidgetSpinner/Assets/Scripts/Shop/MoveSpinners.cs
@@ -2,44 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveSpinners : MonoBehaviour {
-
-    public float currentPosition;
-    int amountOfSpinners;
-    public SpringPanel panel;
-
-    void Start()
-    {
-        currentPosition = transform.localPosition.x;
-        amountOfSpinners = 4;
-
-    }
-    void Update()
+namespace Fidget.Shop
+{
+    public class MoveSpinners : MonoBehaviour
     {
 
-    }
+        public float currentPosition;
+        int amountOfSpinners;
+        public SpringPanel panel;
 
-    public void MoveRight()
-    {
-        currentPosition = panel.target.x;
-        if (currentPosition <= (amountOfSpinners - 1) * -500f)
-            return;
-        currentPosition -= 500f;
-        
-        Vector3 position = new Vector3(currentPosition, 0, 0);
-        SpringPanel.Begin(this.gameObject, position, 8f);
-    }
-    
+        void Start()
+        {
+            currentPosition = transform.localPosition.x;
+            amountOfSpinners = 4;
 
-    public void MoveLeft()
-    {
-        currentPosition = panel.target.x;
-        if (currentPosition >= 0f)
-            return;
-        currentPosition += 500f;
+        }
+        void Update()
+        {
 
-        Vector3 position = new Vector3(currentPosition, 0, 0);
-        SpringPanel.Begin(this.gameObject, position, 8f);
+        }
+
+        public void MoveRight()
+        {
+            currentPosition = panel.target.x;
+            if (currentPosition <= (amountOfSpinners - 1) * -500f)
+                return;
+            currentPosition -= 500f;
+
+            Vector3 position = new Vector3(currentPosition, 0, 0);
+            SpringPanel.Begin(this.gameObject, position, 8f);
+        }
+
+
+        public void MoveLeft()
+        {
+            currentPosition = panel.target.x;
+            if (currentPosition >= 0f)
+                return;
+            currentPosition += 500f;
+
+            Vector3 position = new Vector3(currentPosition, 0, 0);
+            SpringPanel.Begin(this.gameObject, position, 8f);
+        }
+
     }
-    
 }
---
+++ FidgetSpinner/Assets/Scripts/Shop/SpeedGauge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fidget.Shop
+{
+    public class SpeedGauge : MonoBehaviour
+    {
+
+        public Fidget.Common.GaugeUI gauge;
+
+        // Use this for initialization
+        void Start()
+        {
+            gauge.SetGaugeAmount(0.5f);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
