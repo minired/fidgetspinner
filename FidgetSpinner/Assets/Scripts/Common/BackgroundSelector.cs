db4e848a5e447b8549e26a7aebb0169d621048ad
luke <happychrono@gmail.com>
2017-08-02 19:57:20 +0900

    Background Selector 생성, 기본 메테리얼 생성

---
+++ FidgetSpinner/Assets/Scripts/Common/BackgroundSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Fidget.Common
+{
+    public class BackgroundSelector : MonoBehaviour
+    {
+        public List<Material> materialList;
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        public void SetBackground(int index)
+        {
+            gameObject.GetComponent<SpriteRenderer>().material = materialList[index];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
