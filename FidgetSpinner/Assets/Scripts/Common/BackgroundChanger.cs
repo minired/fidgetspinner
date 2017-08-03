2a738ad9837a50790a336d3a25627923982e26df
luke <happychrono@gmail.com>
2017-08-03 14:12:16 +0900

    background changer 테스트

---
+++ FidgetSpinner/Assets/Scripts/Common/BackgroundChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Fidget.Common
+{
+    public class BackgroundChanger : MonoBehaviour
+    {
+        public BackgroundSelector background1;
+        public BackgroundSelector background2;
+        // Use this for initialization
+        void Start()
+        {
+            ChangeBackground(3);
+        }
+
+
+        public void ChangeBackground(int index)
+        {
+            background2.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 20);
+            background2.SetBackground(3);
+            background2.GetComponent<SpriteRenderer>().sortingOrder = background2.GetComponent<SpriteRenderer>().sortingOrder + 1;
+            background2.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 20);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
