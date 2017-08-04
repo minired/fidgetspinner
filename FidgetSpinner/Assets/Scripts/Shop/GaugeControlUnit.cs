46f868c87a6918b4daed70fc95706a3ac840b4dc
DESKTOP-4PBRLFB\KM <sodp2@naver.com>
2017-08-04 21:11:48 +0900

    [shop] 상점에 스피너 2개 추가 및 스피너별 능력치 임의 부여

--- FidgetSpinner/Assets/Scripts/Shop/GaugeControlUnit.cs
+++ FidgetSpinner/Assets/Scripts/Shop/GaugeControlUnit.cs
@@ -14,76 +14,127 @@ namespace Fidget.Shop
             spinner3,
             spinner4,
             spinner5,
+            spinner6,
+            spinner7,
 
             total_spinners
         }
 
-        int currentSpinner;
-        //const float PADDING = 90f;
-
         public SpringPanel panel;
         public UILabel upgradeMoney;
         int int_upgradeMoney;
+        int[] needMoney = new int[(int)Spinners.total_spinners];
+
+        public UILabel currentMoney;
+        int int_currentMoney;
 
         public UISprite speedGauge;
         public UISprite hasteGauge;
         public UISprite dampingGauge;
         public UISprite coinGauge;
+        float[] speedValue = new float[(int)Spinners.total_spinners];
+        float[] hasteValue = new float[(int)Spinners.total_spinners];
+        float[] dampingValue = new float[(int)Spinners.total_spinners];
+        float[] coinValue = new float[(int)Spinners.total_spinners];
+
+        int currentSpinner;
+
+        void Init()
+        {
+            /*TODO: set stats*/
+            for (int i = 0; i < (int)Spinners.total_spinners; ++i)
+            {
+                speedValue[i] = 0.1f * (i + 1);
+                hasteValue[i] = 0.1f * (i + 1);
+                dampingValue[i] = 0.1f * (i + 1);
+                coinValue[i] = 0.1f * (i + 1);
+            }
+        }
 
         void Start()
         {
-            /*
-            speedGauge.fillAmount = 0.3f;
-            hasteGauge.fillAmount = 0.2f;
-            dampingGauge.fillAmount = 0.4f;
-            coinGauge.fillAmount = 0.1f;
-            */
+            Init();
+            int_upgradeMoney = int.Parse(upgradeMoney.text);
+            upgradeMoney.text = int_upgradeMoney.ToString("n0");
         }
 
         void Update()
         {
             currentSpinner = -(int)(Mathf.Round(panel.target.x) / 500);
-            Debug.Log(currentSpinner);
+            //Debug.Log(currentSpinner);
 
             switch(currentSpinner)
             {
                 case (int)Spinners.spinner0:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner0];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner0];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner0];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner0];
                     break;
                 case (int)Spinners.spinner1:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner1];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner1];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner1];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner1];
                     break;
                 case (int)Spinners.spinner2:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner2];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner2];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner2];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner2];
                     break;
                 case (int)Spinners.spinner3:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner3];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner3];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner3];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner3];
                     break;
                 case (int)Spinners.spinner4:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner4];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner4];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner4];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner4];
                     break;
                 case (int)Spinners.spinner5:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner5];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner5];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner5];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner5];
+                    break;
+                case (int)Spinners.spinner6:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner6];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner6];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner6];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner6];
+                    break;
+                case (int)Spinners.spinner7:
+                    speedGauge.fillAmount = speedValue[(int)Spinners.spinner7];
+                    hasteGauge.fillAmount = hasteValue[(int)Spinners.spinner7];
+                    dampingGauge.fillAmount = dampingValue[(int)Spinners.spinner7];
+                    coinGauge.fillAmount = coinValue[(int)Spinners.spinner7];
                     break;
                 default:
                     break;
             }
-
-            //if (((currentPositionX) / 500) == -(int)Spinners.spinner0)
-            //{
-            //    speedGauge.fillAmount = 0.1f;
-            //}
-            //else if(((currentPositionX) / 500) == -(int)Spinners.spinner1)
-            //{
-            //    speedGauge.fillAmount = 0.5f;
-            //}
         }
 
         public void UpgradeStats()
         {
-            int_upgradeMoney = int.Parse(upgradeMoney.text);
+            int_currentMoney = int.Parse(currentMoney.text);
 
-            speedGauge.fillAmount += 0.1f;
-            hasteGauge.fillAmount += 0.1f;
-            dampingGauge.fillAmount += 0.1f;
-            coinGauge.fillAmount += 0.1f;
+            if (int_currentMoney >= int_upgradeMoney)
+            {
+                speedValue[currentSpinner] += 0.1f;
+                hasteValue[currentSpinner] += 0.1f;
+                dampingValue[currentSpinner] += 0.1f;
+                coinValue[currentSpinner] += 0.1f;
 
-            int_upgradeMoney += 500;
-            upgradeMoney.text = int_upgradeMoney.ToString();
+                int_currentMoney -= int_upgradeMoney;
+                int_upgradeMoney += 500;
+                upgradeMoney.text = int_upgradeMoney.ToString("n0");
+
+                currentMoney.text = int_currentMoney.ToString();
+            }
         }
     }
 }
