5b30d04a187fb17a48938ec58eb0de0c5f7668b9
luke <happychrono@gmail.com>
2017-08-10 20:53:12 +0900

    Bottom UI 숨기는 코드 Bottom UI, planet 프리팹 생성

--- FidgetSpinner/Assets/Scripts/Common/BottomUI.cs
+++ FidgetSpinner/Assets/Scripts/Common/BottomUI.cs
@@ -6,11 +6,23 @@ namespace Fidget.Common
 {
     public class BottomUI : MonoBehaviour
     {
+        public GameObject shopBtnObj;
 
+        public GameObject adBtnObj;
+
+
+        Vector3 shopBtnPos;
+        Vector3 adBtnPos;
 
         public void MoveShopScene()
         {
             SceneManager.LoadScene("Shop");
+        }
+
+        private void Awake()
+        {
+            shopBtnPos = shopBtnObj.transform.position;
+            adBtnPos = adBtnObj.transform.position;
         }
 
 
@@ -18,6 +30,35 @@ namespace Fidget.Common
         void Start()
         {
 
+        }
+        void MoveEnd()
+        {
+            shopBtnObj.gameObject.SetActive(false);
+            adBtnObj.gameObject.SetActive(false);
+        }
+
+
+        public void InitPosition()
+        {
+            shopBtnObj.transform.position = shopBtnPos;
+            adBtnObj.transform.position = adBtnPos;
+
+            shopBtnObj.gameObject.SetActive(true);
+            adBtnObj.gameObject.SetActive(true);
+        }
+
+        public void MoveStart()
+        {
+            if (LeanTween.isTweening(shopBtnObj))
+            {
+                LeanTween.cancel(shopBtnObj);
+            }
+            if (LeanTween.isTweening(adBtnObj))
+            {
+                LeanTween.cancel(adBtnObj);
+            }
+            LeanTween.moveX(adBtnObj, -0.9f, 1.0f).setOnComplete(MoveEnd);
+            LeanTween.moveX(shopBtnObj, 0.9f, 1.0f);
         }
 
         // Update is called once per frame
