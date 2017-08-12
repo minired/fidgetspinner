0ab3d73ccc90fd3066432bb270de64f673040bba
LukeKim <happychrono@gmail.com>
2017-08-12 21:06:20 +0900

    샵 레이아웃 추가, 메인씬에 hand Icon 추가

---
+++ FidgetSpinner/Assets/Scripts/TimeGame/MoveHandIcon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Fidget.TimeGame
+{
+    public class MoveHandIcon : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            LeanTween.moveLocalX(gameObject, 70.0f, 1.2f).setEase(LeanTweenType.easeInCirc).setOnComplete(EndAnimation);
+        }
+        
+        void EndAnimation()
+        {
+            Debug.Log("end");
+            LeanTween.moveLocalX(gameObject, 70.0f, 1.2f).setEase(LeanTweenType.easeInCirc).setOnComplete(EndAnimation);
+            //gameObject.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
