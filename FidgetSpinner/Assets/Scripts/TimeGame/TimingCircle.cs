f22e24a678a84f4cc92307c578eb4ddaf6381977
luke <happychrono@gmail.com>
2017-08-15 17:57:22 +0900

    Timing Spin 레이아웃 및 프리팹 구현

---
+++ FidgetSpinner/Assets/Scripts/TimeGame/TimingCircle.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Fidget.TimeGame
+{
+    public class TimingCircle : MonoBehaviour
+    {
+
+        public UISprite timbBar;
+        public UISprite centerCircle;
+
+        public UISprite pivot1;
+        public UISprite pivot2;
+
+        public GameObject particleObj1;
+        public GameObject particleObj2;
+
+
+        float speed = 100f;
+
+        bool isBarSpin = false;
+
+        float distance = 1.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            //SpinStart();
+            StartPaticleAnimation();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Keep us at orbitDistance from target.
+            if (isBarSpin)
+            {
+                timbBar.gameObject.transform.RotateAround(centerCircle.transform.position, Vector3.forward, speed * Time.deltaTime);
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                isBarSpin = !isBarSpin;
+                if (!isBarSpin)
+                {
+                    CheckDistance();
+                }
+
+            }
+        }
+
+        public void CheckDistance()
+        {
+            float d1 = Vector3.Distance(timbBar.transform.position, pivot1.transform.position);
+            float d2 = Vector3.Distance(timbBar.transform.position, pivot2.transform.position);
+            if(d1 < d2)
+            {
+                distance = d1;
+            }
+            else
+            {
+                distance = d2;
+            }
+        }
+
+
+        public float GetPoint()
+        {
+            if(distance < 0.1f)
+            {
+                return 100f - (distance * 80.0f); 
+            }
+            else if(distance < 0.3f)
+            {
+                return 100f - (distance * 120.0f);
+            }
+            else if(distance < 0.4f)
+            {
+                return 100f - (distance * 140.0f);
+            }
+            else
+            {
+                float result = 100f - (distance * 160.0f);
+
+                if(result < 0.0f)
+                {
+                    return 0.0f;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        public void SpinStart()
+        {
+            isBarSpin = true;
+        }
+
+        public void SpinStop()
+        {
+            isBarSpin = false;
+        }
+
+        public void AnimationEnd(GameObject obj)
+        {
+            obj.SetActive(false);
+        }
+
+
+        public void StartPaticleAnimation()
+        {
+            particleObj1.gameObject.SetActive(true);
+            particleObj1.GetComponent<DG.Tweening.DOTweenPath>().DORewind();
+            particleObj1.GetComponent<DG.Tweening.DOTweenPath>().DOPlay();
+
+            particleObj2.gameObject.SetActive(true);
+            particleObj2.GetComponent<DG.Tweening.DOTweenPath>().DORewind();
+            particleObj2.GetComponent<DG.Tweening.DOTweenPath>().DOPlay();
+        }
+    }
+}
