83233ab41fde1bd7cb0816582fa47895f58fe4f8
DESKTOP-4PBRLFB\KM <sodp2@naver.com>
2017-08-15 20:36:46 +0900

    [shop] 장착한 피젯 확인

--- FidgetSpinner/Assets/Scripts/Shop/ShopManager.cs
+++ FidgetSpinner/Assets/Scripts/Shop/ShopManager.cs
@@ -10,10 +10,14 @@ namespace Fidget.Shop
 {
     public class ShopManager : MonoBehaviour
     {
+        private const int GAP = 630;
+
         public BackgroundChanger bgChanger;
-        int currentSpinner;
-        int previousSpinner;
-        public SpringPanel panel;
+        private int currentSpinner;
+        private int previousSpinner;
+
+        public SpringPanel springPanel;
+        public UIPanel uiPanel;
 
         public Fidget.Common.BackUI backUI;
 
@@ -25,12 +29,17 @@ namespace Fidget.Shop
 
         void Start()
         {
-            
+            currentSpinner = User.Instance.EquipIndex;
+
+            springPanel.transform.localPosition = new Vector3(-GAP * currentSpinner, 0, 0);
+            uiPanel.clipOffset = new Vector2(GAP * currentSpinner, 0);
+
+            previousSpinner = 0;
         }
 
         void Update()
         {
-            currentSpinner = -(int)(Mathf.Round(panel.target.x) / 630);
+            currentSpinner = -(int)(Mathf.Round(springPanel.target.x) / GAP);
             
             if (previousSpinner != currentSpinner)
                 UpdateBackground();
