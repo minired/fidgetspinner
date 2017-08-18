7612f53a67dd3f445e0f6d802478686de38bd671
luke <happychrono@gmail.com>
2017-08-18 16:25:42 +0900

    K 까지만 표시

--- FidgetSpinner/Assets/Scripts/Common/DigitChanger.cs
+++ FidgetSpinner/Assets/Scripts/Common/DigitChanger.cs
@@ -18,30 +18,6 @@ namespace Fidget.Common
                 to_str = digit / 1000 + "";
                 if (sosu_point != 0) to_str += "." + sosu_point;
                 to_str += "K";
-                if (digit >= 1000000) //100만 이상일 때
-                {
-
-                    sosu_point = (digit % 1000000) / 100000;
-                    to_str = digit / 1000000 + "";
-                    if (sosu_point != 0) to_str += "." + sosu_point;
-                    to_str += "M";
-
-
-                    if (digit >= 1000000000) //10억 이상일 때
-                    {
-                        sosu_point = (digit % 1000000000) / 100000000;
-                        to_str = digit / 1000000000 + "";
-                        if (sosu_point != 0) to_str += "." + sosu_point;
-                        to_str += "B";
-                        if (digit >= 1000000000000) //1조 이상일 때
-                        {
-                            sosu_point = (digit % 1000000000000) / 100000000000;
-                            to_str = digit / 1000000000000 + "";
-                            if (sosu_point != 0) to_str += "." + sosu_point;
-                            to_str += "T";
-                        }
-                    }
-                }
             }
             
             return to_str;
@@ -57,23 +33,6 @@ namespace Fidget.Common
                 to_str = digit / 1000 + "";
                 if (sosu_point != 0) to_str += "." + sosu_point;
                 to_str += "K";
-                if (digit >= 1000000)
-                {
-
-                    sosu_point = (digit % 1000000) / 100000;
-                    to_str = digit / 1000000 + "";
-                    if (sosu_point != 0) to_str += "." + sosu_point;
-                    to_str += "M";
-
-
-                    if (digit >= 1000000000)
-                    {
-                        sosu_point = (digit % 1000000000) / 100000000;
-                        to_str = digit / 1000000000 + "";
-                        if (sosu_point != 0) to_str += "." + sosu_point;
-                        to_str += "B";
-                    }
-                }
             }
             
             return to_str;
