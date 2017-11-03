19f69205dc4027fb7208c6e57a30391d29ee7f89
luke <happychrono@gmail.com>
2017-11-03 21:36:08 +0900

    하단 광고 영역 확보

---
+++ FidgetSpinner/Assets/Scripts/Common/BottomAd.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+namespace Fidget.Common
+{
+    public class BottomAd : MonoBehaviour
+    {
+        private BannerView bannerView;
+
+        // Use this for initialization
+        void Start()
+        {
+            this.RequestBanner();
+        }
+        private void RequestBanner()
+        {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3026969986215886/2438843858";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+            // Create a 320x50 banner at the top of the screen.
+            bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+            // Create an empty ad request.
+            AdRequest request = new AdRequest.Builder().Build();
+
+            // Load the banner with the request.
+            bannerView.LoadAd(request);
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
