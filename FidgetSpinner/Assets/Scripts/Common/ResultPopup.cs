e2451034109c9975828062979abc4492e2a556eb
LukeKim <happychrono@gmail.com>
2017-11-04 19:01:07 +0900

    Game Result 화면 하단 UI 여백 확보

--- FidgetSpinner/Assets/Scripts/Common/ResultPopup.cs
+++ FidgetSpinner/Assets/Scripts/Common/ResultPopup.cs
@@ -57,9 +57,9 @@ namespace Fidget.Common
             btnBack.transform.localPosition = new Vector3(btnBack.transform.localPosition.x, -720.0f, btnBack.transform.localPosition.z);
             btnRank.transform.localPosition = new Vector3(btnRank.transform.localPosition.x, -720.0f, btnRank.transform.localPosition.z);
             btnShare.transform.localPosition = new Vector3(btnShare.transform.localPosition.x, -720.0f, btnShare.transform.localPosition.z);
-            LeanTween.moveLocalY(btnBack, -463.0f, 0.5f).setEaseOutBounce();
-            LeanTween.moveLocalY(btnRank, -463.0f, 0.5f).setEaseOutBounce().setDelay(0.3f);
-            LeanTween.moveLocalY(btnShare, -463.0f, 0.5f).setEaseOutBounce().setDelay(0.6f);
+            LeanTween.moveLocalY(btnBack, -400.0f, 0.5f).setEaseOutBounce();
+            LeanTween.moveLocalY(btnRank, -400.0f, 0.5f).setEaseOutBounce().setDelay(0.3f);
+            LeanTween.moveLocalY(btnShare, -400.0f, 0.5f).setEaseOutBounce().setDelay(0.6f);
         }
 
 
